0755c9bf7391c368a9bfda524ceb8114b7901603
QuentinPetton <quentin.petton@hotmail.fr>
2024-09-12 19:11:31 +0200

    wip section main

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,25 @@
 export default {
   content: ["./public/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'body': ['Montserrat', 'sans-serif'],
+    },
     colors: {
       'white': '#ECECEC',
+      'backgroundBlur': '#0B081C80',
+      'colorBlack': '#0B081C',
+      'borderColorWhite': '#F2F4F8',
+      'LigtGreyColor': '#FFFFFF4D',
     },
     extend: {
         backgroundImage: {
         'hero': "url(/ressources/Hero_Stack.png)",
+        'gradient-text': 'linear-gradient(-8deg, #ECECEC 0%, #868686 100%))',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 };
 
